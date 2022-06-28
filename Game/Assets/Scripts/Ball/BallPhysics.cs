9105dc43395c8984595c09f05b87c6486464f33b
IAintMichael <86877838+IAintMichael@users.noreply.github.com>
2022-06-29 00:29:43 +0200

    Fixed a ball tbounce glitch with math

--- Game/Assets/Scripts/Ball/BallPhysics.cs
+++ Game/Assets/Scripts/Ball/BallPhysics.cs
@@ -8,6 +8,10 @@ public class BallPhysics : MonoBehaviour
     Vector3 dir;
     public float speed = 5f;
 
+    public float radius;
+
+    public LayerMask notBall;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +21,41 @@ public class BallPhysics : MonoBehaviour
     private void Update()
     {
         transform.Translate(dir.normalized * Time.deltaTime * speed);
+
+        Collider2D collider = Physics2D.OverlapCircle(transform.position, radius, notBall);
+
+        if(collider != null)
+        {
+            CheckCollider(collider);
+        }
+    }
+
+    void CheckCollider(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Paddle"))
+        {
+            Vector2 newDir = transform.position - col.transform.position;
+            if(dir.x > 0)
+            {
+                if(newDir.x > 0)
+                {
+                    return;
+                }
+
+                dir = newDir;
+            } else
+            {
+                if (newDir.x < 0)
+                {
+                    return;
+                }
+                dir = newDir;
+            }
+        }
+        else
+        {
+            dir = new Vector2(dir.x, -dir.y);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -36,5 +75,11 @@ public class BallPhysics : MonoBehaviour
         int xDir = Random.Range(0, 2) == 0 ? 1 : -1;
         Vector2 ballEndVelocity = new Vector2(xDir, Random.Range(-1f, 1f));
         return ballEndVelocity;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
     }
 }
